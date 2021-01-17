ad94a842674a7b22834fc2b9a1aee1b49391a157
Toki Horii <toki.h@x-point-1.net>
2021-01-17 18:51:26 +0900

    固定ページテンプレート作成

---
+++ wordpress/wp-content/themes/web_test/page.php
@@ -0,0 +1,104 @@
+<?php get_header(); ?>
+
+<div id="contents">
+  <div id="conL">
+    <h2>サービス内容</h2>
+    <p>貴社ますますご盛栄のこととお喜び申し上げます。<br />
+平素はひとかたならぬご厚情にあずかり、厚くお礼申し上げます。<br />
+弊社ホームページをご閲覧いただき誠にありがとうございます。<br />
+弊社は、ホームページ制作会社として2011年に創業いたしました。　おかげさまで今期で◯期を迎える事ができました。<br />　お客様の感動するものをご提供する。ということをモットーにこれからも尽力してまいります。<br />
+なにかご不明な店がございましたらお気軽にお問い合わせくださいませ。</p>
+
+    <h3 id="s1">ホームページ制作事業部</h3>
+    <p>弊社はお客様と一緒に最高のホームページの創ってまいります。</p>
+
+    <h4>料金体系</h4>
+    <table>
+      <tr>
+        <th>ホームページ制作基本料 </th>
+        <td>45,000円～</td>
+      </tr>
+      <tr>
+        <th>企画費</th>
+        <td>40,000円～</td>
+      </tr>
+      <tr>
+        <th>デザイン費</th>
+        <td>50,000円～</td>
+      </tr>
+      <tr>
+        <th>トップページ制作代</th>
+        <td>65,000円～</td>
+      </tr>
+      <tr>
+        <th>階層ページ</th>
+        <td>15,000円～</td>
+      </tr>
+      <tr>
+        <th>サーバ料金</th>
+        <td>別途 (サーバ費用弊社委託の場合はお問い合わせください）</td>
+      </tr>
+    </table>
+    <p>製作期間はボリュームによって変動いたします。まずはお問い合わせください。</p>
+
+    <h3 id="s2">集客代行事業部</h3>
+    <p>SEO対策の効果を体験してみたい方に最適な「ベーシックプラン」をご用意いたしました。<br />
+    プロフェッショナルプラン」もご用意しておりますので、ご自由に選択いただけます。</p>
+
+    <table>
+      <tr>
+        <th>項目 </th>
+        <th>ベーシックプラン </th>
+        <th>プロフェッショナルプラン </th>
+      </tr>
+      <tr>
+        <th>初期費用</th>
+        <td>40,000円～</td>
+        <td>40,000円～</td>
+      </tr>
+      <tr>
+        <th>月額費用</th>
+        <td>19,800円/月</td>
+        <td>29,800円/月</td>
+      </tr>
+      <tr>
+        <th>ホームページ最適化</th>
+        <td>対象ページの基本構成を最適化します</td>
+        <td>対象ページの基本構成を最適化します</td>
+      </tr>
+      <tr>
+        <th>目標順位</th>
+        <td>20位以内</td>
+        <td>20位以内</td>
+      </tr>
+      <tr>
+        <th>最低契約期間</th>
+        <td>3ヶ月</td>
+        <td>6ヶ月</td>
+      </tr>
+    </table>
+
+    <a href="contact"><button class="btn1">お問い合わせ</button></a>
+  </div><!-- /#conL -->
+
+  <div id="conR">
+    <div class="submenu">
+        <h3>事業内容</h3>
+        <h4>ネット事業</h4>
+        <ul>
+            <li><a href="<?php home_url(); ?>/service#s1">ホームページ制作</a></li>
+            <li><a href="<?php home_url(); ?>/service#s2">広告代理</a></li>
+
+        </ul>
+    </div><!-- /.submenu -->
+
+    <div class="bnrR">
+      <ul>
+          <li><a href="<?php home_url(); ?>/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/bnr_r.jpg" alt="" /></a></li>
+          <li><a href="<?php home_url(); ?>/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/bnr_r.jpg" alt="" /></a></li>
+      </ul>
+    </div><!-- /.bnrR -->
+  </div><!-- /#conR -->
+</div><!-- /#contents -->
+
+<?php get_footer();
