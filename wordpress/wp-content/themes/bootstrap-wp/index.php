10cc0b5e9de6f07463ea6b3591254d3e9ae404bf
Toki Horii <toki.h@x-point-1.net>
2021-02-07 21:53:57 +0900

    index.htmlをphpに変換

--- wordpress/wp-content/themes/bootstrap-wp/index.php
+++ wordpress/wp-content/themes/bootstrap-wp/index.php
@@ -11,15 +11,24 @@
   <title>Clean Blog - Start Bootstrap Theme</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link
+    href="<?php echo get_template_directory_uri(); ?>/vendor/bootstrap/css/bootstrap.min.css"
+    rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+  <link
+    href="<?php echo get_template_directory_uri(); ?>/vendor/fontawesome-free/css/all.min.css"
+    rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet'
+    type='text/css'>
+  <link
+    href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+    rel='stylesheet' type='text/css'>
 
   <!-- Custom styles for this template -->
-  <link href="css/clean-blog.min.css" rel="stylesheet">
+  <link
+    href="<?php echo get_template_directory_uri(); ?>/css/clean-blog.min.css"
+    rel="stylesheet">
 
 </head>
 
@@ -29,7 +38,9 @@
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
       <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+        data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+        aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
       </button>
@@ -82,7 +93,8 @@
           </a>
           <p class="post-meta">Posted by
             <a href="#">Start Bootstrap</a>
-            on September 24, 2019</p>
+            on September 24, 2019
+          </p>
         </div>
         <hr>
         <div class="post-preview">
@@ -93,7 +105,8 @@
           </a>
           <p class="post-meta">Posted by
             <a href="#">Start Bootstrap</a>
-            on September 18, 2019</p>
+            on September 18, 2019
+          </p>
         </div>
         <hr>
         <div class="post-preview">
@@ -107,7 +120,8 @@
           </a>
           <p class="post-meta">Posted by
             <a href="#">Start Bootstrap</a>
-            on August 24, 2019</p>
+            on August 24, 2019
+          </p>
         </div>
         <hr>
         <div class="post-preview">
@@ -121,7 +135,8 @@
           </a>
           <p class="post-meta">Posted by
             <a href="#">Start Bootstrap</a>
-            on July 8, 2019</p>
+            on July 8, 2019
+          </p>
         </div>
         <hr>
         <!-- Pager -->
