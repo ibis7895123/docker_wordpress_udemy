33d86df1804b226d0bf0146c4a0c9376884e5e5b
Toki Horii <toki.h@x-point-1.net>
2021-02-14 19:56:09 +0900

    タイトルを変更

--- wordpress/wp-content/themes/bootstrap-wp/archive.php
+++ wordpress/wp-content/themes/bootstrap-wp/archive.php
@@ -17,8 +17,8 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
-            <h1>Clean Blog</h1>
-            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+            <h1>Category</h1>
+            <span class="subheading"><?php wp_title(''); ?></span>
           </div>
         </div>
       </div>
