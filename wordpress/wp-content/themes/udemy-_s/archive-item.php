94e6620875ba2b188bbc3b9b53a1b2eceaaa453d
Toki Horii <toki.h@x-point-1.net>
2021-03-07 14:56:39 +0900

    タイトル修正

--- wordpress/wp-content/themes/udemy-_s/archive-item.php
+++ wordpress/wp-content/themes/udemy-_s/archive-item.php
@@ -15,13 +15,8 @@ get_header();
     <?php if (have_posts()) : ?>
 
     <header class="page-header">
-        <?php
-                the_archive_title('<h1 class="page-title">', '</h1>');
-                the_archive_description('<div class="archive-description">', '</div>');
-                ?>
+        <h1>商品リスト</h1>
     </header><!-- .page-header -->
-
-    <h1>archive-item</h1>
 
     <?php
             /* Start the Loop */
@@ -34,17 +29,14 @@ get_header();
                  * called content-___.php (where ___ is the Post Type name) and that will be used instead.
                  */
                 get_template_part('template-parts/content', get_post_type());
-
             endwhile;
 
             the_posts_navigation();
-
         else :
-
             get_template_part('template-parts/content', 'none');
 
         endif;
-        ?>
+    ?>
 
 </main><!-- #main -->
 
