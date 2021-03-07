db4186aeac6aced4c52470510737729f57dcc702
Toki Horii <toki.h@x-point-1.net>
2021-03-07 15:35:07 +0900

    ジャンル一覧のナビを作成

--- wordpress/wp-content/themes/udemy-_s/taxonomy-genre.php
+++ wordpress/wp-content/themes/udemy-_s/taxonomy-genre.php
@@ -16,29 +16,44 @@ get_header();
 
   <header class="page-header">
     <h1>商品リスト</h1>
+    <?php
+        $terms = get_terms('genre');
+    ?>
+    <?php if ($terms): ?>
+    <ul class="nav">
+      <?php foreach ($terms as $term): ?>
+      <li class="nav-item">
+        <a class="nav-link"
+          href="<?php echo get_term_link($term); ?>">
+          <?php echo esc_html($term->name); ?>
+        </a>
+      </li>
+      <?php endforeach; ?>
+    </ul>
+    <?php endif; ?>
     <h2>
       <?php the_archive_title(); ?>
     </h2>
   </header><!-- .page-header -->
 
   <?php
-        /* Start the Loop */
-        while (have_posts()):
-            the_post();
+      /* Start the Loop */
+      while (have_posts()):
+        the_post();
 
-            /*
-              * Include the Post-Type-specific template for the content.
-              * If you want to override this in a child theme, then include a file
-              * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-              */
-            get_template_part('template-parts/content', get_post_type());
-        endwhile;
+        /*
+          * Include the Post-Type-specific template for the content.
+          * If you want to override this in a child theme, then include a file
+          * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+          */
+        get_template_part('template-parts/content', get_post_type());
+      endwhile;
 
-            the_posts_navigation();
-        else:
-            get_template_part('template-parts/content', 'none');
+        the_posts_navigation();
+      else:
+        get_template_part('template-parts/content', 'none');
 
-        endif;
+      endif;
     ?>
 
 </main><!-- #main -->
